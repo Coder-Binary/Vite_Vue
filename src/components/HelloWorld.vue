bfe7a8b11c1279379e8e374172dd452296b48fd0
ABin <Admin@ABin.Pro>
2022-08-10 15:15:29 +0800

    初始化仓库

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+    data(){
+        return {
+            count: 1 ,
+            msg: 'Vite + Vue',
+        }
+    },
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+    <!-- <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <el-button>中文</el-button> -->
+
+  <div class="card">
+    <el-button type="primary" round @click="this.count = this.count * 2" > count is {{ count }}</el-button>
+<el-scrollbar height="400px">
+    <p v-for="item in 200" :key="item" class="scrollbar-demo-item">{{ item }}</p>
+  </el-scrollbar>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
+}
+</style>
